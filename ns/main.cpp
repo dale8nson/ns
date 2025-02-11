a1bda164bf13fea459bd632f4e17b608ad63027f
Dale Hutchinson <daletristanhutchinson@gmail.com>
2025-02-11 16:40:56 +1100

    Initial Commit

---
+++ ns/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ns
+//
+//  Created by Dale Hutchinson on 11/2/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
